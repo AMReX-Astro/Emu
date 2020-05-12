a5eb16c64d6557dfd4ab082df7ce2ce6c1cb799d
Sherwood Richers <srichers@caltech.edu>
2020-05-11 20:47:56 -0700

    bipolar test works! (if you change theta12 to 0.01)

--- Scripts/visualization/plot_first_particle.py
+++ Scripts/visualization/plot_first_particle.py
@@ -166,26 +166,32 @@ if __name__ == "__main__":
 
     t = []
     fee = []
+    fexR = []
+    fexI = []
     fxx = []
-    feebar = []
-    fxxbar = []
     pupt = []
 
     files = sorted(glob.glob("plt*"))
-
+    print(files[0], files[-1])
+    
     for f in files:
         
         plotfile = f #"plt"+str(i).zfill(5)
         idata, rdata = read_particle_data(plotfile, ptype="neutrinos")
         p = rdata[0]
         t.append(p[rkey["time"]])
-        fee.append(p[rkey["f01_Re"]])
-        fxx.append(p[rkey["f01_Im"]])
-        feebar.append(p[rkey["f01_Rebar"]])
-        fxxbar.append(p[rkey["f01_Imbar"]])
+        fee.append(p[rkey["f00_Re"]])
+        fexR.append(p[rkey["f01_Re"]])
+        fexI.append(p[rkey["f01_Im"]])
+        fxx.append(p[rkey["f11_Re"]])
         pupt.append(p[rkey["pupt"]])
 
+    fee = np.array(fee)
+    fexR = np.array(fexR)
+    fexI = np.array(fexI)
+    fxx = np.array(fxx)
     t = np.array(t)
+    print(t)
 
     # The neutrino energy we set
     #E = dm21c4 * np.sin(2.*theta12) / (8.*np.pi*hbar*clight)
@@ -204,17 +210,20 @@ if __name__ == "__main__":
     fig = plt.gcf()
     fig.set_size_inches(8, 8)
 
-    plt.plot(t*clight, fee, 'b-',linewidth=0.5)
+    plt.plot(t, fee, 'b-',linewidth=0.5,label="f00_Re")
+    plt.plot(t, fexR, 'g-',linewidth=0.5,label="f01_Re")
+    plt.plot(t, fexI, 'r-',linewidth=0.5,label="f01_Im")
+    plt.plot(t, fxx, 'k-',linewidth=0.5,label="f11_Re")
+
+    x = fexR
+    y = fexI
+    z = 0.5*(fee-fxx)
+    plt.plot(t, np.sqrt(x**2+y**2+z**2),linewidth=0.5,label="radius")
     
-    plt.plot(t*clight, fxx, 'g-',linewidth=0.5)
-
-    #plt.plot(t*clight, feebar, 'r-',linewidth=0.5)
-
-    #plt.plot(t*clight, fxxbar, 'k-',linewidth=0.5)
-
     plt.grid()
+    plt.legend()
     #plt.axis((0., 1., 0., 1.))
     ax = plt.gca()
-    ax.set_xlabel(r'$ct$ (cm)')
+    ax.set_xlabel(r'$t$ (cm)')
     ax.set_ylabel(r'$f$')
     plt.savefig('single_neutrino.png')
