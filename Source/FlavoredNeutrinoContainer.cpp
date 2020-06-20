ae4bc323db4b7ebafdb8e612aa870a4b9e92e76e
Donald E. Willcox <eugene.willcox@gmail.com>
2020-06-19 20:51:32 -0700

    Modify IntegrateParticles() to take a particle container containing the neutrino RHS. Also add utility function for updating particle locations in a RHS particle container to the corresponding particle locations for the neutrino state data.

--- Source/FlavoredNeutrinoContainer.cpp
+++ Source/FlavoredNeutrinoContainer.cpp
@@ -6,7 +6,7 @@
 using namespace amrex;
 
 void FlavoredNeutrinoContainer::
-IntegrateParticles(const Real dt)
+IntegrateParticles(const FlavoredNeutrinoContainer& neutrinos_rhs, const Real dt)
 {
     BL_PROFILE("FlavoredNeutrinoContainer::IntegrateParticles");
 
@@ -15,18 +15,80 @@ IntegrateParticles(const Real dt)
     const auto dxi = Geom(lev).InvCellSizeArray();
     const auto plo = Geom(lev).ProbLoArray();
 
+    FNParIter pti_F(*this, lev);
+    FNParIter pti_dFdt(neutrinos_rhs, lev);
+
+    auto checkValid = [&]() -> bool {
+        bool f_v = pti_F.isValid();
+        bool df_v = pti_dFdt.isValid();
+        AMREX_ASSERT(f_v == df_v);
+        return f_v && df_v;
+    };
+
+    auto ptIncrement = [&](){ ++pti_F; ++pti_dFdt; };
+
 #ifdef _OPENMP
 #pragma omp parallel
 #endif
-    for (FNParIter pti(*this, lev); pti.isValid(); ++pti)
+    for (; checkValid(); ptIncrement())
     {
-        const int np  = pti.numParticles();
-        ParticleType * pstruct = &(pti.GetArrayOfStructs()[0]);
+        const int np_F  = pti_F.numParticles();
+        const int np_dFdt  = pti_dFdt.numParticles();
+        AMREX_ASSERT(np_F == np_dFdt);
 
-        ParallelFor ( np, [=] AMREX_GPU_DEVICE (int i) {
-            ParticleType& p = pstruct[i];
-            integrate_particle(p, dt);
-        });       
+        ParticleType* ps_F = &(pti_F.GetArrayOfStructs()[0]);
+        ParticleType* ps_dFdt = &(pti_dFdt.GetArrayOfStructs()[0]);
+
+        ParallelFor (np_F, [=] AMREX_GPU_DEVICE (int i) {
+            ParticleType& p_F = ps_F[i];
+            ParticleType& p_dFdt = ps_dFdt[i];
+            integrate_particle(p_F, p_dFdt, dt);
+        });
+    }
+}
+
+void FlavoredNeutrinoContainer::
+UpdateLocationRHS(FlavoredNeutrinoContainer& neutrinos_rhs)
+{
+    BL_PROFILE("FlavoredNeutrinoContainer::UpdateLocationRHS");
+
+    const int lev = 0;
+
+    const auto dxi = Geom(lev).InvCellSizeArray();
+    const auto plo = Geom(lev).ProbLoArray();
+
+    FNParIter pti_F(*this, lev);
+    FNParIter pti_dFdt(neutrinos_rhs, lev);
+
+    auto checkValid = [&]() -> bool {
+        bool f_v = pti_F.isValid();
+        bool df_v = pti_dFdt.isValid();
+        AMREX_ASSERT(f_v == df_v);
+        return f_v && df_v;
+    };
+
+    auto ptIncrement = [&](){ ++pti_F; ++pti_dFdt; };
+
+#ifdef _OPENMP
+#pragma omp parallel
+#endif
+    for (; checkValid(); ptIncrement())
+    {
+        const int np_F  = pti_F.numParticles();
+        const int np_dFdt  = pti_dFdt.numParticles();
+        AMREX_ASSERT(np_F == np_dFdt);
+
+        ParticleType* ps_F = &(pti_F.GetArrayOfStructs()[0]);
+        ParticleType* ps_dFdt = &(pti_dFdt.GetArrayOfStructs()[0]);
+
+        ParallelFor (np_F, [=] AMREX_GPU_DEVICE (int i) {
+            ParticleType& p_F = ps_F[i];
+            ParticleType& p_dFdt = ps_dFdt[i];
+
+            p_dFdt.pos(0) = p_F.pos(0);
+            p_dFdt.pos(1) = p_F.pos(1);
+            p_dFdt.pos(2) = p_F.pos(2);
+        });
     }
 }
 
