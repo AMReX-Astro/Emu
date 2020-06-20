2d561539b0e79eef1c25232118d9530dbd174b4f
Donald E. Willcox <eugene.willcox@gmail.com>
2020-06-20 01:00:34 -0700

    Fix some compile issues and revise the update location function for flexibility.

--- Source/FlavoredNeutrinoContainer.cpp
+++ Source/FlavoredNeutrinoContainer.cpp
@@ -48,46 +48,46 @@ IntegrateParticles(FlavoredNeutrinoContainer& neutrinos_rhs, const Real dt)
 }
 
 void FlavoredNeutrinoContainer::
-UpdateLocationRHS(FlavoredNeutrinoContainer& neutrinos_rhs)
+UpdateLocationFrom(FlavoredNeutrinoContainer& Other)
 {
-    BL_PROFILE("FlavoredNeutrinoContainer::UpdateLocationRHS");
+    BL_PROFILE("FlavoredNeutrinoContainer::UpdateLocationFrom");
 
     const int lev = 0;
 
     const auto dxi = Geom(lev).InvCellSizeArray();
     const auto plo = Geom(lev).ProbLoArray();
 
-    FNParIter pti_F(*this, lev);
-    FNParIter pti_dFdt(neutrinos_rhs, lev);
+    FNParIter pti_this(*this, lev);
+    FNParIter pti_other(Other, lev);
 
     auto checkValid = [&]() -> bool {
-        bool f_v = pti_F.isValid();
-        bool df_v = pti_dFdt.isValid();
-        AMREX_ASSERT(f_v == df_v);
-        return f_v && df_v;
+        bool this_v = pti_this.isValid();
+        bool other_v = pti_other.isValid();
+        AMREX_ASSERT(this_v == other_v);
+        return this_v && other_v;
     };
 
-    auto ptIncrement = [&](){ ++pti_F; ++pti_dFdt; };
+    auto ptIncrement = [&](){ ++pti_this; ++pti_other; };
 
 #ifdef _OPENMP
 #pragma omp parallel
 #endif
     for (; checkValid(); ptIncrement())
     {
-        const int np_F  = pti_F.numParticles();
-        const int np_dFdt  = pti_dFdt.numParticles();
-        AMREX_ASSERT(np_F == np_dFdt);
+        const int np_this  = pti_this.numParticles();
+        const int np_other = pti_other.numParticles();
+        AMREX_ASSERT(np_this == np_other);
 
-        ParticleType* ps_F = &(pti_F.GetArrayOfStructs()[0]);
-        ParticleType* ps_dFdt = &(pti_dFdt.GetArrayOfStructs()[0]);
+        ParticleType* ps_this = &(pti_this.GetArrayOfStructs()[0]);
+        ParticleType* ps_other = &(pti_other.GetArrayOfStructs()[0]);
 
-        ParallelFor (np_F, [=] AMREX_GPU_DEVICE (int i) {
-            ParticleType& p_F = ps_F[i];
-            ParticleType& p_dFdt = ps_dFdt[i];
+        ParallelFor (np_this, [=] AMREX_GPU_DEVICE (int i) {
+            ParticleType& p_this = ps_this[i];
+            ParticleType& p_other = ps_other[i];
 
-            p_dFdt.pos(0) = p_F.pos(0);
-            p_dFdt.pos(1) = p_F.pos(1);
-            p_dFdt.pos(2) = p_F.pos(2);
+            p_this.pos(0) = p_other.pos(0);
+            p_this.pos(1) = p_other.pos(1);
+            p_this.pos(2) = p_other.pos(2);
         });
     }
 }
