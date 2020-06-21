345720075f1a827fd4f88d368fb1d10bec54be80
Donald E. Willcox <eugene.willcox@gmail.com>
2020-06-20 18:07:35 -0700

    Remove FlavoredNeutrinoContainer::IntegrateParticles since we use the new integrator interface.

--- Source/FlavoredNeutrinoContainer.cpp
+++ Source/FlavoredNeutrinoContainer.cpp
@@ -6,48 +6,6 @@
 using namespace amrex;
 
 void FlavoredNeutrinoContainer::
-IntegrateParticles(FlavoredNeutrinoContainer& neutrinos_rhs, const Real dt)
-{
-    BL_PROFILE("FlavoredNeutrinoContainer::IntegrateParticles");
-
-    const int lev = 0;
-
-    const auto dxi = Geom(lev).InvCellSizeArray();
-    const auto plo = Geom(lev).ProbLoArray();
-
-    FNParIter pti_F(*this, lev);
-    FNParIter pti_dFdt(neutrinos_rhs, lev);
-
-    auto checkValid = [&]() -> bool {
-        bool f_v = pti_F.isValid();
-        bool df_v = pti_dFdt.isValid();
-        AMREX_ASSERT(f_v == df_v);
-        return f_v && df_v;
-    };
-
-    auto ptIncrement = [&](){ ++pti_F; ++pti_dFdt; };
-
-#ifdef _OPENMP
-#pragma omp parallel
-#endif
-    for (; checkValid(); ptIncrement())
-    {
-        const int np_F  = pti_F.numParticles();
-        const int np_dFdt  = pti_dFdt.numParticles();
-        AMREX_ASSERT(np_F == np_dFdt);
-
-        ParticleType* ps_F = &(pti_F.GetArrayOfStructs()[0]);
-        ParticleType* ps_dFdt = &(pti_dFdt.GetArrayOfStructs()[0]);
-
-        ParallelFor (np_F, [=] AMREX_GPU_DEVICE (int i) {
-            ParticleType& p_F = ps_F[i];
-            const ParticleType& p_dFdt = ps_dFdt[i];
-            integrate_particle(p_F, p_dFdt, dt);
-        });
-    }
-}
-
-void FlavoredNeutrinoContainer::
 UpdateLocationFrom(FlavoredNeutrinoContainer& Other)
 {
     BL_PROFILE("FlavoredNeutrinoContainer::UpdateLocationFrom");
