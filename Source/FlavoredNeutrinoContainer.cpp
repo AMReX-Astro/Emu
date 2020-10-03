957367a03e19c64a449129c04414a6fcfa599794
Donald E. Willcox <eugene.willcox@gmail.com>
2020-10-03 15:37:00 -0700

    fix bug where particle iterators were not thread-private by simplifying the logic here -- since the two particle containers in UpdateLocationFrom() will have the same particle, grid, and tile layout, we can use the same particle iterator for both by pulling the (grid, tile) indexes from the particle iterator and accessing the corresponding particle tiles with ParticlesAt().

--- Source/FlavoredNeutrinoContainer.cpp
+++ Source/FlavoredNeutrinoContainer.cpp
@@ -13,33 +13,27 @@ UpdateLocationFrom(FlavoredNeutrinoContainer& Ploc)
     const auto dxi = Geom(lev).InvCellSizeArray();
     const auto plo = Geom(lev).ProbLoArray();
 
-    FNParIter pti_this(*this, lev);
-    FNParIter pti_ploc(Ploc, lev);
-
-    auto checkValid = [&]() -> bool {
-        bool this_v = pti_this.isValid();
-        bool ploc_v = pti_ploc.isValid();
-        AMREX_ASSERT(this_v == ploc_v);
-        return this_v && ploc_v;
-    };
-
-    auto ptIncrement = [&](){ ++pti_this; ++pti_ploc; };
-
 #ifdef _OPENMP
 #pragma omp parallel
 #endif
-    for (; checkValid(); ptIncrement())
+    for (FNParIter pti(*this, lev); pti.isValid(); ++pti)
     {
-        const int np_this = pti_this.numParticles();
-        const int np_ploc = pti_ploc.numParticles();
-        AMREX_ASSERT(np_this == np_ploc);
+        auto grid_tile = pti.GetPairIndex();
 
-        ParticleType* ps_this = &(pti_this.GetArrayOfStructs()[0]);
-        ParticleType* ps_ploc = &(pti_ploc.GetArrayOfStructs()[0]);
+        auto& this_tile = this->ParticlesAt(lev, grid_tile.first, grid_tile.second);
+        auto& ploc_tile = Ploc.ParticlesAt(lev, grid_tile.first, grid_tile.second);
 
-        ParallelFor (np_this, [=] AMREX_GPU_DEVICE (int i) {
+        AMREX_ASSERT(this_tile.numParticles() == ploc_tile.numParticles());
+        int np = this_tile.numParticles();
+
+        ParticleType* ps_this = &(this_tile.GetArrayOfStructs()[0]);
+        ParticleType* ps_ploc = &(ploc_tile.GetArrayOfStructs()[0]);
+
+        amrex::ParallelFor (np, [=] AMREX_GPU_DEVICE (int i) {
             ParticleType& p_this = ps_this[i];
             ParticleType& p_ploc = ps_ploc[i];
+
+            AMREX_ASSERT(p_this.id() == p_ploc.id());
 
             p_this.pos(0) = p_ploc.rdata(PIdx::x);
             p_this.pos(1) = p_ploc.rdata(PIdx::y);
