c5dcb9ed927b60bba66b15377d91599a37b7328e
Donald E. Willcox <eugene.willcox@gmail.com>
2020-10-04 14:37:56 -0700

    Revise location reset strategy since Reset() is not supported on GPUs and we can let Redistribute() handle the periodicity.

--- Source/FlavoredNeutrinoContainer.cpp
+++ Source/FlavoredNeutrinoContainer.cpp
@@ -4,9 +4,11 @@
 using namespace amrex;
 
 void FlavoredNeutrinoContainer::
-ResetLocationInPlace()
+SyncLocation(int type)
 {
-    BL_PROFILE("FlavoredNeutrinoContainer::ResetLocationInPlace");
+    BL_PROFILE("FlavoredNeutrinoContainer::SyncLocation");
+
+    AMREX_ASSERT(type==Sync::CoordinateToPosition || type==Sync::PositionToCoordinate);
 
     const int lev = 0;
 
@@ -21,21 +23,17 @@ ResetLocationInPlace()
         amrex::ParallelFor (np, [=] AMREX_GPU_DEVICE (int i) {
             ParticleType& p = pstruct[i];
 
-            // Copy integrated position to the particle position.
-            p.pos(0) = p.rdata(PIdx::x);
-            p.pos(1) = p.rdata(PIdx::y);
-            p.pos(2) = p.rdata(PIdx::z);
-
-            // Checks if particle is out of domain bounds, and if so,
-            // applies the geometry periodicity attempting to shift it
-            // back into the domain. Invalidates the particle if unsuccessful
-            // by negating its ID, to be deleted on the next Redistribute().
-            Reset(p, true);
-
-            // Copy the reset particle position back to the integrated position.
-            p.rdata(PIdx::x) = p.pos(0);
-            p.rdata(PIdx::y) = p.pos(1);
-            p.rdata(PIdx::z) = p.pos(2);
+            if (type == Sync::CoordinateToPosition) {
+                // Copy integrated position to the particle position.
+                p.pos(0) = p.rdata(PIdx::x);
+                p.pos(1) = p.rdata(PIdx::y);
+                p.pos(2) = p.rdata(PIdx::z);
+            } else if (type == Sync::PositionToCoordinate) {
+                // Copy the reset particle position back to the integrated position.
+                p.rdata(PIdx::x) = p.pos(0);
+                p.rdata(PIdx::y) = p.pos(1);
+                p.rdata(PIdx::z) = p.pos(2);
+            }
         });
     }
 }
@@ -46,8 +44,7 @@ UpdateLocationFrom(FlavoredNeutrinoContainer& Ploc)
     // This function updates particle locations in the current particle container
     // using the particle locations in particle container Ploc.
     //
-    // Because it is assumed that ResetLocationInPlace() has been called for Ploc,
-    // we also copy the particle id's so particles invalidated in Ploc are
+    // We also copy the particle id's so particles invalidated in Ploc are
     // invalidated for the current particle container as well.
     BL_PROFILE("FlavoredNeutrinoContainer::UpdateLocationFrom");
 
@@ -75,9 +72,9 @@ UpdateLocationFrom(FlavoredNeutrinoContainer& Ploc)
 
             AMREX_ASSERT(p_this.id() == p_ploc.id() || p_this.id() == -p_ploc.id());
 
-            p_this.pos(0) = p_ploc.rdata(PIdx::x);
-            p_this.pos(1) = p_ploc.rdata(PIdx::y);
-            p_this.pos(2) = p_ploc.rdata(PIdx::z);
+            p_this.pos(0) = p_ploc.pos(0);
+            p_this.pos(1) = p_ploc.pos(1);
+            p_this.pos(2) = p_ploc.pos(2);
 
             // if the particle has been invalidated in Ploc, invalidate it here also
             if (p_ploc.id() < 0) {
