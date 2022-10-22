7333787164f598b2e89bb2bf88e856db4dd34bb4
Sherwood Richers <srichers@caltech.edu>
2022-10-22 07:35:43 -0700

    function to create a new simulation

--- Scripts/runtools/runtools.py
+++ Scripts/runtools/runtools.py
@@ -1,10 +1,13 @@
+import os
+import shutil
+
 # write an input file in a particular directory with a set of inputs
-def print_inputfile(par_dict, directory):
-    filename = directory+"/inputs"
-    f = open(filename,"w")
-    for key in par_dict:
+def write_inputfile(directory, inputs_dict, filename="inputs"):
+    path = directory+"/"+filename
+    f = open(path,"w")
+    for key in inputs_dict:
         f.write(key+" = ")
-        value = par_dict[key]
+        value = inputs_dict[key]
         wrapper = "\"" if type(value)==str else ""
         f.write(wrapper)
         if type(value)==list:
@@ -16,8 +19,59 @@ def print_inputfile(par_dict, directory):
                 f.write(str(elem))
             f.write(")")
         else:
-            f.write(str(par_dict[key]))
+            f.write(str(inputs_dict[key]))
         f.write(wrapper)
         f.write("\n")
     f.close()
 
+# given a run directory and an optional inputs filename,
+# return a dictionary with key-value pairs for the parameters
+# keys and values are both returned as strings
+def read_inputfile(directory, filename="inputs"):
+    path = directory + "/"+filename
+    f = open(path,"r")
+    inputs_dict = {}
+    for line in f.readlines():
+        # remove comments
+        line = line.split("#")[0]
+
+        # skip empty lines
+        if "=" not in line:
+            continue
+
+        # split line into key/value
+        key,value = line.split("=")
+        for character in ["\""," ","\n"]:
+            value = value.replace(character,"")
+
+        # append to the dictionary
+        inputs_dict[key] = value
+
+    f.close()
+    return inputs_dict
+
+# set up a new simulation in the specified location with an inputs dictionary and executable
+# Refuse to overwrite unless overwrite==True
+# note that the simulation still lacks a particle file! Must be created separately.
+def new_simulation(directory, inputs_dict, executable_path, overwrite=False):
+
+    # check whether the directory already exiss
+    if os.path.exists(directory):
+        if not overwrite:
+            raise(directory+" already exists. Cannot create ")
+        else:
+            os.system('rm -rf '+directory)
+
+    # create the new directories
+    os.mkdir(directory)
+    subdir = directory+"/run0"
+    os.mkdir(subdir)
+
+    # copy in the executable
+    os.system("cp "+executable_path+" "+subdir)
+
+    # write the inputs file
+    write_inputfile(subdir, inputs_dict)
+
+
+    
