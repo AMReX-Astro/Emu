d01edc027ad434b27d96af4b7a78def81baf2c4b
Sherwood Richers <srichers@berkeley.edu>
2022-10-22 10:41:52 -0700

    added the ignore_pos optional argument for the rkey dictionary. I must have missed this while I was merging the two different versions together

--- Scripts/data_reduction/amrex_plot_tools.py
+++ Scripts/data_reduction/amrex_plot_tools.py
@@ -9,7 +9,7 @@ eV = 1.60218e-12 # erg
 mp = 1.6726219e-24 # g
 GF = 1.1663787e-5 / (1e9*eV)**2 * (hbar*clight)**3 #erg cm^3
 
-def get_basic_particle_keys():
+def get_basic_particle_keys(ignore_pos=False):
     real_quantities = ["pos_x",
                        "pos_y",
                        "pos_z",
@@ -22,7 +22,8 @@ def get_basic_particle_keys():
                        "pupz",
                        "pupt"]
     rkey = {}
-    for i, rlabel in enumerate(real_quantities):
+    start = 3 if ignore_pos else 0
+    for i, rlabel in enumerate(real_quantities[start:]):
         rkey[rlabel] = i
 
     ikey = {
@@ -31,7 +32,7 @@ def get_basic_particle_keys():
 
     return rkey, ikey
 
-def get_particle_keys():
+def get_particle_keys(ignore_pos=False):
     real_quantities = ["pos_x",
                        "pos_y",
                        "pos_z",
@@ -57,7 +58,8 @@ def get_particle_keys():
                        "f11_Rebar"]
 
     rkey = {}
-    for i, rlabel in enumerate(real_quantities):
+    start = 3 if ignore_pos else 0
+    for i, rlabel in enumerate(real_quantities[start:]):
         rkey[rlabel] = i
 
     ikey = {
@@ -66,7 +68,7 @@ def get_particle_keys():
 
     return rkey, ikey
 
-def get_3flavor_particle_keys():
+def get_3flavor_particle_keys(ignore_pos=False):
     real_quantities = ["pos_x",
                        "pos_y",
                        "pos_z",
@@ -102,7 +104,8 @@ def get_3flavor_particle_keys():
                        "f22_Rebar"]
 
     rkey = {}
-    for i, rlabel in enumerate(real_quantities):
+    start = 3 if ignore_pos else 0
+    for i, rlabel in enumerate(real_quantities[start:]):
         rkey[rlabel] = i
 
     ikey = {
