c6365fe108999fcad9cd78f81c4b26b1d5ac74b0
Sherwood Richers <richers@utk.edu>
2024-04-19 15:00:50 -0400

    improve babysitting script to also plot the ELN

--- Scripts/babysitting/avgfee_HDF5.py
+++ Scripts/babysitting/avgfee_HDF5.py
@@ -24,8 +24,21 @@ I=1
 # read averaged data #
 ######################
 avgData = h5py.File("reduced0D.h5","r")
+
+NF = 0
+if "N00(1|ccm)" in avgData.keys():
+    NF = 2
+if "N11(1|ccm)" in avgData.keys():
+    NF = 3
+assert(NF==2 or NF==3)
+
 t=np.array(avgData["time(s)"])*1e9
 N00=np.array(avgData["N00(1|ccm)"])
+N11=np.array(avgData["N11(1|ccm)"])
+N22=np.array(avgData["N22(1|ccm)"])
+N00bar=np.array(avgData["N00bar(1|ccm)"])
+N11bar=np.array(avgData["N11bar(1|ccm)"])
+N22bar=np.array(avgData["N22bar(1|ccm)"])
 Noffdiag = np.array(avgData["N_offdiag_mag(1|ccm)"])
 avgData.close()
 
@@ -76,3 +89,30 @@ ax.grid(which='both')
 
 ax.semilogy(t, Noffdiag)
 plt.savefig("avgfemu.pdf", bbox_inches="tight")
+
+# plot ELN
+plt.cla()
+ax.set_ylabel(r"$\delta$ELN/$N_\mathrm{tot}$")
+ax.set_xlabel(r"$t\,(10^{-9}\,\mathrm{s})$")
+ax.tick_params(axis='both', which='both', direction='in', right=True,top=True)
+ax.xaxis.set_minor_locator(AutoMinorLocator())
+ax.yaxis.set_minor_locator(AutoMinorLocator())
+ax.minorticks_on()
+ax.grid(which='both')
+
+Ntot = N00+N11+N00bar+N11bar
+if NF==3:
+    Ntot += N22+N22bar
+
+emu = (N00-N00bar) - (N11-N11bar)
+ax.plot(t, (emu-emu[0])/Ntot,label=r"$e\mu$")
+
+etau = (N00-N00bar) - (N22-N22bar)
+ax.plot(t, (etau-etau[0])/Ntot,label=r"$e\tau$")
+
+if NF==3:
+    mutau = (N11-N11bar) - (N22-N22bar)
+    ax.plot(t, (mutau-mutau[0])/Ntot,label=r"$\mu\tau$")
+
+plt.legend()
+plt.savefig("ELN.pdf", bbox_inches="tight")
