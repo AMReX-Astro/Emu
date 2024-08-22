0353cce247e8841407c0c98be661bf979d4adbd9
erickurquilla1999 <erickurquillla1999@gmail.com>
2024-08-22 16:35:37 -0400

    Creating two separate scripts to compute the energy bin size for collision tests:
    Now, instead of a single script to compute the energy bin size, I have included two separate scripts—one for the collisional instability test and another for the particles-to-equilibrium test.

--- Scripts/collisions/compute_dE_coll_equi_test.py
+++ Scripts/collisions/compute_dE_coll_equi_test.py
@@ -1,12 +1,12 @@
 '''
-This script compute the energy bin size ( /Delta E ) for monoenergetic neutrino simulations and the antineutrino chemical potential, given:
+This script compute the energy bin size ( /Delta E ) for monoenergetic neutrino simulations given:
 - Number of neutrinos at equilibrium
 - Volume of a cell
 - Number of momentum beams isotropically distributed per cell
 - Neutrinos energy bin center
 - Neutrino chemical potential
 - Background matter temperature
-This script was used to compute the energy bin size in the test scripts and antineutrino chemical potential: coll_equi_test.py and coll_inst_test.py.
+This script was used to compute the energy bin size in the test scripts: coll_equi_test.py.
 '''
 
 import numpy as np
@@ -18,13 +18,13 @@ c = 2.99792458e10 # cm/s
 hc = h * c # erg cm
 
 # Simulation parameters 
-V = 1 # Volume of a cell ( ccm ) 
+V = 10**3 # Volume of a cell ( ccm ) 
 Ndir = 92 # Number of momentum beams isotropically distributed per cell 
-E = 20.0 # Neutrinos and antineutrinos energy bin center ( Mev )
-T = 7.0 # Background matter temperature ( Mev )
+E = 50.0 # Neutrinos and antineutrinos energy bin center ( Mev )
+T = 10.0 # Background matter temperature ( Mev )
 
-N_eq_electron_neutrino = 3.260869565e+31 # Number of electron neutrinos at equilibrium
-u_electron_neutrino = 20.0 # Electron neutrino chemical potential ( Mev )
+N_eq_electron_neutrino = 1e33 # Number of electron neutrinos at equilibrium
+u_electron_neutrino = 0.0 # Electron neutrino chemical potential ( Mev )
 
 # Fermi-dirac distribution factor for electron neutrinos
 f_eq_electron_neutrinos = 1 / ( 1 + np.exp( ( E - u_electron_neutrino ) / T ) ) # adimentional
@@ -45,11 +45,3 @@ for complex_deltaE in deltaE:
     if (np.imag(complex_deltaE)==0):
         print(f'Delta energy bin in MeV = {np.real(complex_deltaE)}')
         dE=np.real(complex_deltaE)
-
-# Electron neutrino flavor
-N_eq_electron_antineutrino = 2.717391304e+31 # Number of electron antineutrinos at equilibrium
-
-# Computing electron antineutrino chemical potential
-f_eq_electron_antineutrino = 3 * N_eq_electron_antineutrino * ( hc )**3 / ( V * ( 4 * np.pi / Ndir ) * delta_E_cubic )
-u_electron_antineutrino = E - T * np.log( 1 / f_eq_electron_antineutrino - 1 )
-print(f'Electron neutrino chemical potential in MeV = {u_electron_antineutrino}')
