53b986ff0fe0bf5e78b0e357cb9d7f80a1535cc8
erickurquilla1999 <erickurquilla1999@gmail.com>
2024-09-20 17:44:05 -0400

    Stop the code if the number of cells and domain size in the HDF5 background file do not agree with the number of cells and domain size in the parameter file.

--- Source/ReadInput_RhoTempYe.cpp
+++ Source/ReadInput_RhoTempYe.cpp
@@ -17,9 +17,32 @@ void set_rho_T_Ye(MultiFab& state, const Geometry& geom, const TestParams* parms
     ReadInputRhoYeT(hdf5_background_rho_Ye_T_name);
 
     using namespace background_input_rho_T_Ye;
+    int ncell_x = *n_cell_x;
     int ncell_y = *n_cell_y;
     int ncell_z = *n_cell_z;
+    double xmin_ = *x_min;
+    double xmax_ = *x_max;
+    double ymin_ = *y_min;
+    double ymax_ = *y_max;
+    double zmin_ = *z_min;
+    double zmax_ = *z_max;
+    double lx = xmax_ - xmin_;
+    double ly = ymax_ - ymin_;
+    double lz = zmax_ - zmin_;
 
+    amrex::Print() << "ncell_x = " << ncell_x << std::endl;
+    amrex::Print() << "parms->ncell[0] = " << parms->ncell[0] << std::endl;
+    
+    if (ncell_x != parms->ncell[0] || ncell_y != parms->ncell[1] || ncell_z != parms->ncell[2]) {
+      amrex::Print() << "The number of cells in the background data file does not match the parameter file" << std::endl;
+      abort();
+    }
+    
+    if (lx != parms->Lx || ly != parms->Ly || lz != parms->Lz ) {
+      amrex::Print() << "The simulation domain in the background data file does not match the parameter file" << std::endl;
+      abort();
+    }
+    
     rhoYeT_input_struct rhoYeT_input_obj(rho_array_input, Ye_array_input, T_array_input);
 
     for(amrex::MFIter mfi(rho_T_ye_state); mfi.isValid(); ++mfi){
