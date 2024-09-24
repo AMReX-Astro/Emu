2704addb8ebeefc5d3f0338369ca90e63f149501
erickurquilla1999 <erickurquilla1999@gmail.com>
2024-09-24 10:54:21 -0400

    Modify the Python script that generates the initial conditions for the collisional instability test to compute the phase space volume.
    Warning: This modification is not passing the test.

--- Scripts/initial_conditions/st8_coll_inst_test.py
+++ Scripts/initial_conditions/st8_coll_inst_test.py
@@ -1,4 +1,9 @@
-import h5py
+'''
+Created by Erick Urquilla, Department of Physics and Astronomy, University of Tennessee, Knoxville.
+This script is used to create the particle initial conditions that attempt to replicate the 
+simulation in the paper Collisional Flavor Instabilities of Supernova Neutrinos by L. Johns 
+[2104.11369].
+'''
 import numpy as np
 import sys
 import os
@@ -8,29 +13,65 @@ sys.path.append(importpath+"/../data_reduction")
 from initial_condition_tools import uniform_sphere, moment_interpolate_particles, minerbo_interpolate, write_particles
 import amrex_plot_tools as amrex
 
-# These initial conditions are intended to replicate the collisional instability outputs in "Collisional Flavor Instabilities of Supernova Neutrinos" by L. Johns [2104.11369].
-# Simulation parameters
-NF = 2
-nphi_equator = 16
-nnue = 3.0e+33 # 1/ccm
-nnua = 2.5e+33 # 1/ccm
-nnux = 1.0e+33 # 1/ccm
-fnue = np.array([0.0 , 0.0 , 0.0])
-fnua = np.array([0.0 , 0.0 , 0.0])
-fnux = np.array([0.0 , 0.0 , 0.0])
-energy_erg = 20.0 # MeV
-energy_erg *= 1e6*amrex.eV # erg 
+# These initial conditions are intended to replicate the collisional instability outputs in
+# "Collisional Flavor Instabilities of Supernova Neutrinos" by L. Johns [2104.11369].
 
-nnu = np.zeros((2,NF))
-nnu[0,0] = nnue
-nnu[1,0] = nnua
-nnu[:,1:] = nnux
+NF = 2 # Number of flavors
+nphi_equator = 16 # number of direction in equator ---> theta = pi/2
 
-fnu = np.zeros((2,NF,3))
-fnu[0,0,:] = nnue * fnue
-fnu[1,0,:] = nnua * fnua
-fnu[:,1:,:] = nnu[:,1:,np.newaxis] * fnux[np.newaxis,np.newaxis,:]
+nu_e = 3.0e+33 # 1/ccm
+nu_x = 1.0e+33 # 1/ccm
+nu_ebar = 2.5e+33 # 1/ccm
+nu_xbar = 1.0e+33 # 1/ccm
 
-particles = moment_interpolate_particles(nphi_equator, nnu, fnu, energy_erg, uniform_sphere, minerbo_interpolate) # [particle, variable]
+# Energy bin size
+energy_bin_size_MeV = 2.272540842052914 # Energy in Mev
 
+# Energy bin centers extracted from NuLib table
+energies_center_Mev = [20.0] # Energy in Mev
+# Energy bin bottom extracted from NuLib table
+energies_bottom_Mev = [20.0-energy_bin_size_MeV/2.0]
+# Energy bin top extracted from NuLib table
+energies_top_Mev = [20.0+energy_bin_size_MeV/2.0]
+
+# Energies in ergs
+energies_center_erg = np.array(energies_center_Mev) * 1e6*amrex.eV # Energy in ergs
+energies_bottom_erg = np.array(energies_bottom_Mev) * 1e6*amrex.eV # Energy in ergs
+energies_top_erg    = np.array(energies_top_Mev   ) * 1e6*amrex.eV # Energy in ergs
+
+# Generate the number of energy bins
+n_energies = len(energies_center_erg)
+
+# Get variable keys
+rkey, ikey = amrex.get_particle_keys(NF, ignore_pos=True)
+
+# Generate the number of variables that describe each particle
+n_variables = len(rkey)
+
+# Get the momentum distribution of the particles
+phat = uniform_sphere(nphi_equator)
+
+# Generate the number of directions
+n_directions = len(phat)
+
+# Generate the number of particles
+n_particles = n_energies * n_directions
+
+# Initialize a NumPy array to store all particles
+particles = np.zeros((n_energies, n_directions, n_variables))
+
+# Fill the particles array using a loop, replacing append
+for i, energy_bin in enumerate(energies_center_erg):
+    particles[i , : , rkey["pupx"] : rkey["pupz"]+1 ] = energy_bin * phat
+    particles[i , : , rkey["pupt"]                  ] = energy_bin
+    particles[i , : , rkey["Vphase"]                ] = ( 4.0 * np.pi / n_directions ) * ( ( energies_top_erg[i] ** 3 - energies_bottom_erg[i] ** 3 ) / 3.0 )
+    particles[i , : , rkey["N00_Re"]                ] = nu_e
+    particles[i , : , rkey["N11_Re"]                ] = nu_x
+    particles[i , : , rkey["N00_Rebar"]             ] = nu_ebar
+    particles[i , : , rkey["N11_Rebar"]             ] = nu_xbar
+
+# Reshape the particles array
+particles = particles.reshape(n_energies * n_directions, n_variables)
+
+# Write particles initial condition file
 write_particles(np.array(particles), NF, "particle_input.dat")
