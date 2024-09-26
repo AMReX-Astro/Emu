05fc808310561b1706f262a7ebd92da9b11a0daf
erickurquilla1999 <erickurquillla1999@gmail.com>
2024-09-26 17:02:44 -0400

    Creating an input for the boundary condition type.

--- Source/main.cpp
+++ Source/main.cpp
@@ -45,8 +45,7 @@ void evolve_flavor(const TestParams* parms)
     //Option 0: use periodic BC
     //Option 1: create particles at boundary.
 
-    //FIXME: FIXME: Define this in parameter file.
-    const int BC_type = 0; //0=periodic, 1=outer.
+    const int BC_type = parm->boundary_condition_type; //0=periodic, 1=outer.
 
     int BC_type_val;
     enum BC_type_enum {PERIODIC, OUTER};
