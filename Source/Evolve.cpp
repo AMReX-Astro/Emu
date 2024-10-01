964ed4d3a65b1054e49faf0fedae96d11631da70
erickurquilla1999 <erickurquilla1999@gmail.com>
2024-10-01 17:37:09 -0400

    Solve syntax issue.

--- Source/Evolve.cpp
+++ Source/Evolve.cpp
@@ -199,7 +199,7 @@ void interpolate_rhs_from_mesh(FlavoredNeutrinoContainer& neutrinos_rhs, const M
             if(parms->do_nsm==1 ){
             
                 // Compute particle distance from black hole center
-                double particle_distance_from_bh_center = pow( pow( p.rdata(PIdx::x) - parms->bh_center_x , 2.0 ) + pow( p.rdata(PIdx::y) - parms->bh_center_y , 2.0 ) + pow( p.rdata(PIdx::z) - parms->bh_center_z , 2.0 ) , 0.5 ); #cm
+                double particle_distance_from_bh_center = pow( pow( p.rdata(PIdx::x) - parms->bh_center_x , 2.0 ) + pow( p.rdata(PIdx::y) - parms->bh_center_y , 2.0 ) + pow( p.rdata(PIdx::z) - parms->bh_center_z , 2.0 ) , 0.5 ); //cm
 
                 // Set time derivatives to zero if particles is inside the BH
                 if ( particle_distance_from_bh_center < parms->bh_radius ) {
