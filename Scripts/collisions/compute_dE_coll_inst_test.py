f5e9e712fdc5e24ea66957a463828ae27fdb432e
erickurquilla1999 <erickurquilla1999@gmail.com>
2024-11-25 11:12:52 -0500

    Improve script that compute dE and antineutrino chemical potential for collisional flavor instability test

--- Scripts/collisions/compute_dE_coll_inst_test.py
+++ Scripts/collisions/compute_dE_coll_inst_test.py
@@ -18,16 +18,19 @@ c = 2.99792458e10 # cm/s
 hc = h * c # erg cm
 
 # Simulation parameters 
-V = 1 # Volume of a cell ( ccm ) 
+V = (1.0e3)**3 # Volume of a cell ( ccm ) 
 Ndir = 92 # Number of momentum beams isotropically distributed per cell 
 E = 20.0 # Neutrinos and antineutrinos energy bin center ( Mev )
 T = 7.0 # Background matter temperature ( Mev )
 
-N_eq_electron_neutrino = 3.260869565e+31 # Number of electron neutrinos at equilibrium
+N_eq_electron_neutrino_density = 3.0e33 # Density of electron neutrinos at equilibrium  (ccm)
+N_eq_electron_neutrino = N_eq_electron_neutrino_density * V / Ndir # Number of electron neutrinos at equilibrium per beam
+print(f'Number of electron neutrinos at equilibrium per beam = {N_eq_electron_neutrino}')
 u_electron_neutrino = 20.0 # Electron neutrino chemical potential ( Mev )
 
 # Fermi-dirac distribution factor for electron neutrinos
 f_eq_electron_neutrinos = 1 / ( 1 + np.exp( ( E - u_electron_neutrino ) / T ) ) # adimentional
+print(f'Fermi-Dirac distribution factor for electron neutrinos = {f_eq_electron_neutrinos}')
 
 # We know : 
 #   dE^3      = 3 *          Neq           * ( hc )^ 3 / ( dV *        dOmega       *        feq              )
@@ -47,9 +50,13 @@ for complex_deltaE in deltaE:
         dE=np.real(complex_deltaE)
 
 # Electron neutrino flavor
-N_eq_electron_antineutrino = 2.717391304e+31 # Number of electron antineutrinos at equilibrium
+N_eq_electron_antineutrino_density = 2.5e33 # Density of electron antineutrinos at equilibrium (ccm)
+N_eq_electron_antineutrino = N_eq_electron_antineutrino_density * V / Ndir # Number of electron antineutrinos at equilibrium per beam
+print(f'Number of electron antineutrinos at equilibrium per beam = {N_eq_electron_antineutrino}')
+Vphase = V * ( 4 * np.pi / Ndir ) * delta_E_cubic  / 3
+print(f'Phase space volume in ccm MeV^3 = {Vphase}')
 
 # Computing electron antineutrino chemical potential
 f_eq_electron_antineutrino = 3 * N_eq_electron_antineutrino * ( hc )**3 / ( V * ( 4 * np.pi / Ndir ) * delta_E_cubic )
 u_electron_antineutrino = E - T * np.log( 1 / f_eq_electron_antineutrino - 1 )
-print(f'Electron neutrino chemical potential in MeV = {u_electron_antineutrino}')
+print(f'Electron antineutrino chemical potential in MeV = {u_electron_antineutrino}')
