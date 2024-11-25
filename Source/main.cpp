501fe694c04a9f7935dee23a335e3163c5e0b1dd
erickurquilla1999 <erickurquilla1999@gmail.com>
2024-11-25 15:07:25 -0500

    Add an if statement to compute the IMFP MultiFab only when the time step method is zero

--- Source/main.cpp
+++ Source/main.cpp
@@ -140,7 +140,11 @@ void evolve_flavor(const TestParams* parms)
     }
    
     // Compute the maximum Inverse Mean Free Path (IMFP) in the domain
-    MultiFab max_IMFP_absortion = compute_max_IMFP(geom, state, parms);
+    MultiFab max_IMFP_absortion;
+    max_IMFP_absortion.setVal(0.0);
+    if (parms->time_step_method == 0) {
+        max_IMFP_absortion = compute_max_IMFP(geom, state, parms);
+    }
 
     // Initialize particles on the domain
     amrex::Print() << "Initializing particles... " << std::endl;
