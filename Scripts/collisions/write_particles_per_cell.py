bfbba289203ea14377c419b5313b7c6885370030
erickurquilla1999 <erickurquilla1999@gmail.com>
2025-01-04 17:28:37 -0500

    Update write particles per cell script to recive the grid index as input parameter

--- Scripts/collisions/write_particles_per_cell.py
+++ Scripts/collisions/write_particles_per_cell.py
@@ -19,6 +19,7 @@ import amrex_plot_tools as amrex
 import emu_yt_module as emu
 from multiprocessing import Pool
 import argparse
+import time
 
 class GridData(object):
     def __init__(self, ad):
@@ -77,72 +78,76 @@ def writehdf5files(dire):
     nlevels = len(header.grids)
     assert nlevels==1
     level = 0
-    ngrids = len(header.grids[level])
 
-    # loop over all cells within each grid
-    for gridID in range(ngrids):
-        # read particle data on a single grid
-        idata, rdata = amrex.read_particle_data(dire, ptype="neutrinos", level_gridID=(level,gridID))
-        
-        # get cell index for each particle
-        cell_x_index = (rdata[:,rkey['pos_x']] / grid_data.dx).astype(int)
-        cell_y_index = (rdata[:,rkey['pos_y']] / grid_data.dy).astype(int)
-        cell_z_index = (rdata[:,rkey['pos_z']] / grid_data.dz).astype(int)
-        cell_index = np.stack((cell_x_index, cell_y_index, cell_z_index), axis=1)
-        # get unique cell index
-        unique_cell_index = np.unique(cell_index, axis=0)
-
-        for i in range(unique_cell_index.shape[0]):
-            cell_filename = f"{dire}_particles/cell_{unique_cell_index[i,0]}_{unique_cell_index[i,1]}_{unique_cell_index[i,2]}.h5"              
-            if os.path.exists(cell_filename):
-                print(f"file {cell_filename} already exists")
-            else:
-                mask_cell_group = np.all(cell_index == unique_cell_index[i], axis=1)
-                cell_group = rdata[mask_cell_group]
-                with h5py.File(cell_filename, 'w') as cell_hf:
-                    for label in labels:
-                        cell_hf.create_dataset(label, data=cell_group[:, rkey[label]], maxshape=(None,), chunks=True)
-
-    return dire
-
-# run the write hdf5 files function in parallel
-if __name__ == '__main__':
-
-    dir_particles_binary = sorted(glob.glob("plt*/neutrinos"))
-    dir_particles_binary = [dir_particles_binary[i].split('/')[0] for i in range(len(dir_particles_binary))] # remove "neutrinos"
-    dir_particles_hdf5 = sorted(glob.glob("plt*_particles"))
-    dir_particles_hdf5 = [dir_particles_hdf5[i].split('_')[0] for i in range(len(dir_particles_hdf5))]
-    directories = [d for d in dir_particles_binary if d not in dir_particles_hdf5]
+    gridID = grid_i + grid_data.nx*grid_j + grid_data.nx*grid_data.ny*grid_k
     
-    if not directories:
-        print("No new directories to process.")
-        sys.exit(0)
- 
-    parser = argparse.ArgumentParser(description='Process some directories.')
-    parser.add_argument('--index', type=int, default=-2, help='index of the directory to analyze')
-    args = parser.parse_args()
+    # read particle data on a single grid
+    idata, rdata = amrex.read_particle_data(dire, ptype="neutrinos", level_gridID=(level,gridID))
+    
+    # get cell index for each particle
+    cell_x_index = (rdata[:,rkey['pos_x']] / grid_data.dx).astype(int)
+    cell_y_index = (rdata[:,rkey['pos_y']] / grid_data.dy).astype(int)
+    cell_z_index = (rdata[:,rkey['pos_z']] / grid_data.dz).astype(int)
+    cell_index = np.stack((cell_x_index, cell_y_index, cell_z_index), axis=1)
+    # get unique cell index
+    unique_cell_index = np.unique(cell_index, axis=0)
 
-    if args.index >= -1:
-        if args.index == -1:
-            directories = [directories[-1]]
-        elif args.index < len(directories):
-            directories = [directories[args.index]]
+    for i in range(unique_cell_index.shape[0]):
+        cell_filename = f"{dire}_particles/cell_{unique_cell_index[i,0]}_{unique_cell_index[i,1]}_{unique_cell_index[i,2]}.h5"              
+        if os.path.exists(cell_filename):
+            print(f"file {cell_filename} already exists")
         else:
-            print(f"Index {args.index} is out of range. Exiting.")
-            sys.exit(1)
+            mask_cell_group = np.all(cell_index == unique_cell_index[i], axis=1)
+            cell_group = rdata[mask_cell_group]
+            with h5py.File(cell_filename, 'w') as cell_hf:
+                for label in labels:
+                    cell_hf.create_dataset(label, data=cell_group[:, rkey[label]], maxshape=(None,), chunks=True)
 
-    print(f"Directories to process: {directories}")
+dir_particles_binary = sorted(glob.glob("plt*/neutrinos"))
+dir_particles_binary = [dir_particles_binary[i].split('/')[0] for i in range(len(dir_particles_binary))] # remove "neutrinos"
+dir_particles_hdf5 = sorted(glob.glob("plt*_particles"))
+dir_particles_hdf5 = [dir_particles_hdf5[i].split('_')[0] for i in range(len(dir_particles_hdf5))]
+directories = [d for d in dir_particles_binary if d not in dir_particles_hdf5]
 
-    # get NF
-    eds = emu.EmuDataset(directories[0])
-    NF = eds.get_num_flavors()
-    if NF==2:
-        rkey, ikey = amrex.get_particle_keys(NF)
-        labels=['pos_x','pos_y','pos_z', 'time', 'x', 'y', 'z', 'pupx', 'pupy', 'pupz', 'pupt', 'N00_Re', 'N01_Re', 'N01_Im', 'N11_Re', 'N00_Rebar', 'N01_Rebar', 'N01_Imbar', 'N11_Rebar', 'TrHN', 'Vphase']
-    if NF==3:
-        rkey, ikey = amrex.get_particle_keys(NF)
-        labels=['pos_x','pos_y','pos_z','time','x', 'y', 'z', 'pupx', 'pupy', 'pupz', 'pupt', 'N00_Re', 'N01_Re', 'N01_Im', 'N02_Re', 'N02_Im', 'N11_Re', 'N12_Re', 'N12_Im' ,'N22_Re', 'N00_Rebar', 'N01_Rebar', 'N01_Imbar', 'N02_Rebar', 'N02_Imbar', 'N11_Rebar', 'N12_Rebar' ,'N12_Imbar', 'N22_Rebar', 'TrHN', 'Vphase']
+if not directories:
+    print("No new directories to process.")
+    sys.exit(0)
 
-    nproc = mp.cpu_count()
-    pool = Pool(nproc)
-    finalresult = pool.map(writehdf5files, directories)
+parser = argparse.ArgumentParser(description='Process some directories.')
+parser.add_argument('--index', type=int, default=-2, help='index of the directory to analyze')
+parser.add_argument('--grid_i', type=int, required=True, help='grid index in the x direction')
+parser.add_argument('--grid_j', type=int, required=True, help='grid index in the y direction')
+parser.add_argument('--grid_k', type=int, required=True, help='grid index in the z direction')
+args = parser.parse_args()
+
+if args.index >= -1:
+    if args.index == -1:
+        directories = [directories[-1]]
+    elif args.index < len(directories):
+        directories = [directories[args.index]]
+    else:
+        print(f"Index {args.index} is out of range. Exiting.")
+        sys.exit(1)
+
+grid_i = args.grid_i
+grid_j = args.grid_j
+grid_k = args.grid_k
+
+print(f"Directories to process: {directories}")
+
+# get NF
+eds = emu.EmuDataset(directories[0])
+NF = eds.get_num_flavors()
+if NF==2:
+    rkey, ikey = amrex.get_particle_keys(NF)
+    labels=['pos_x','pos_y','pos_z', 'time', 'x', 'y', 'z', 'pupx', 'pupy', 'pupz', 'pupt', 'N00_Re', 'N01_Re', 'N01_Im', 'N11_Re', 'N00_Rebar', 'N01_Rebar', 'N01_Imbar', 'N11_Rebar', 'TrHN', 'Vphase']
+if NF==3:
+    rkey, ikey = amrex.get_particle_keys(NF)
+    labels=['pos_x','pos_y','pos_z','time','x', 'y', 'z', 'pupx', 'pupy', 'pupz', 'pupt', 'N00_Re', 'N01_Re', 'N01_Im', 'N02_Re', 'N02_Im', 'N11_Re', 'N12_Re', 'N12_Im' ,'N22_Re', 'N00_Rebar', 'N01_Rebar', 'N01_Imbar', 'N02_Rebar', 'N02_Imbar', 'N11_Rebar', 'N12_Rebar' ,'N12_Imbar', 'N22_Rebar', 'TrHN', 'Vphase']
+
+start_time_gpu = time.time()
+nproc = mp.cpu_count()
+pool = Pool(nproc)
+pool.map(writehdf5files, directories)
+end_time_gpu = time.time()
+print(f"Processing time: {end_time_gpu - start_time_gpu} seconds")
