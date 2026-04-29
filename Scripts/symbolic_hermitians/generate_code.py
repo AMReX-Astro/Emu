d14bbbd804b1560fab2fd638eaba36a0b82e7683
erickurquilla <erickurq@gmail.com>
2026-04-29 14:40:03 -0400

    setting the derivative of the equilibrium distribution function to zero to not change it in time integration

--- Scripts/symbolic_hermitians/generate_code.py
+++ Scripts/symbolic_hermitians/generate_code.py
@@ -512,6 +512,13 @@ if __name__ == "__main__":
         TrHN = (H*N).trace();
         code.append(["p.rdata(PIdx::TrHN) += ("+sympy.cxxcode(sympy.simplify(TrHN))+");"])
 
+    # Looping over neutrinos(tail: no tail) and antineutrinos(tail: bar)
+    for t in tails:
+
+        N_equilibrium = HermitianMatrix(args.N, "p.rdata(PIdx::N{}{}_{}"+t+"_eq)") # Neutrino number matrix
+        N_equilibrium.H = N_equilibrium.H * 0.0
+        code.append(N_equilibrium.code())
+
     code = [line for sublist in code for line in sublist]
     write_code(code, os.path.join(args.emu_home, "Source/generated_files", "Evolve.cpp_dfdt_fill"))
 
