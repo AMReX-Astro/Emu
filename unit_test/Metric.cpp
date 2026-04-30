648994ffdcaaa2b12c3a00ff696ec55077a30fb5
Debraj <dkundu2@vols.utk.edu>
2026-04-30 18:01:08 -0400

    changed Metric.cpp to test all the coordinate systems instead of taking user choice of coordinate system

--- unit_test/Metric.cpp
+++ unit_test/Metric.cpp
@@ -54,36 +54,6 @@ void Euler( EParticle& p, Metric &metric, double dt, int steps)
 
 int main()
 {   
-    //chosing the coordinate system from user input
-
-    std::string coord;
-
-    std::cout << "Enter coordinate system:";
-    std::cin >> coord;
-
-    assert(
-    coord == "cartesian" ||
-    coord == "cylindrical" ||
-    coord == "spherical"
-    );
-
-    CartesianMetric cart_metric;
-    CylindricalMetric cyl_metric;
-    SphericalMetric sph_metric;
-
-    Metric *met = nullptr;
-    
-    if (coord == "cartesian") {
-        met = &cart_metric;
-    }
-    else if (coord == "cylindrical") {
-        met = &cyl_metric;
-    }
-    else if (coord == "spherical") {
-        met = &sph_metric;
-    }
-
-    Metric &metric= *met;
     
     //declaring and initializing the particle
     EParticle p;
@@ -98,20 +68,34 @@ int main()
     p.rdata(PIdx::pupx)= 2.0;
     p.rdata(PIdx::pupy)= 1.0;
     p.rdata(PIdx::pupz)= 2.0;
+
+    //duplicate initial particle
+    EParticle p1;
+
+    p1=p;
     
+
     //time step setup
     double t= 5;
     int steps= 10000;
     double dt= t/steps;
     double v;
     
-    Euler(p, metric, dt, steps); //Euler does the time integration
+
+
+    //Cartesian test
+    //****************
+
+    CartesianMetric metric1;
+    
+    Euler(p, metric1, dt, steps); //Euler does the time integration
     
     // calculating volume. Syntax: vol(xmax,xmin,ymax,ymin,zmax,zmin). 
-    v=metric.vol(4,2,3,-1,5,1);  
+    v=metric1.vol(4,2,3,-1,5,1);  
     
-    double tol = coord == "cartesian" ? 1e-12 : 1e-2;
-
+    double tol = 1e-12; //setting tolerance for assert
+    
+    //asserting the correct final result
     assert(std::abs(p.rdata(PIdx::time)- 5.0) < tol);
     assert(std::abs(p.rdata(PIdx::x) - 13.0 / 3.0) < tol);
     assert(std::abs(p.rdata(PIdx::y) - 8.0 / 3.0) < tol);
@@ -120,53 +104,111 @@ int main()
     assert(std::abs(p.rdata(PIdx::pupx) - 2.0) < tol);
     assert(std::abs(p.rdata(PIdx::pupy) - 1.0) < tol);
     assert(std::abs(p.rdata(PIdx::pupz) - 2.0) < tol);
+    assert(std::abs(v - 32.0) < 1e-3);
     
     // Printing out final state values and volume
-    if( coord=="cartesian" )
-    {
-        std::cout << "final values in cartesian\n";
-        std::cout << "time = " << p.rdata(PIdx::time)  << "\n";
-        std::cout << "x    = " << p.rdata(PIdx::x)    << "\n";
-        std::cout << "y    = " << p.rdata(PIdx::y)    << "\n";
-        std::cout << "z    = " << p.rdata(PIdx::z)    << "\n";
-        std::cout << "pt   = " << p.rdata(PIdx::pupt)  << "\n";
-        std::cout << "px   = " << p.rdata(PIdx::pupx) << "\n";
-        std::cout << "py   = " << p.rdata(PIdx::pupy) << "\n";
-        std::cout << "pz   = " << p.rdata(PIdx::pupz) << "\n";
-        std::cout << "v   = " << v << "\n";
-    }
+    std::cout << "final values in cartesian coordinates\n";
+    std::cout << "time = " << p.rdata(PIdx::time)  << "\n";
+    std::cout << "x    = " << p.rdata(PIdx::x)    << "\n";
+    std::cout << "y    = " << p.rdata(PIdx::y)    << "\n";
+    std::cout << "z    = " << p.rdata(PIdx::z)    << "\n";
+    std::cout << "pt   = " << p.rdata(PIdx::pupt)  << "\n";
+    std::cout << "px   = " << p.rdata(PIdx::pupx) << "\n";
+    std::cout << "py   = " << p.rdata(PIdx::pupy) << "\n";
+    std::cout << "pz   = " << p.rdata(PIdx::pupz) << "\n";
+    std::cout << "v   = " << v << "\n";
+    
 
-    if( coord=="cylindrical" || coord=="spherical" )
-    {   
-        metric.coord_conv(p);
-        std::cout << "final values in "<<" "<<coord<<"\n";
-        std::cout << "time = " << p.rdata(PIdx::time)  << "\n";
-        std::cout << "x    = " << p.rdata(PIdx::x)    << "\n";
-        std::cout << "y    = " << p.rdata(PIdx::y)    << "\n";
-        std::cout << "z    = " << p.rdata(PIdx::z)    << "\n";
-        std::cout << "pt   = " << p.rdata(PIdx::pupt)  << "\n";
-        std::cout << "px   = " << p.rdata(PIdx::pupx) << "\n";
-        std::cout << "py   = " << p.rdata(PIdx::pupy) << "\n";
-        std::cout << "pz   = " << p.rdata(PIdx::pupz) << "\n";
-        std::cout << "v   = " << v << "\n";
-        
-        metric.coord_conv_inv(p);
-
-        std::cout << "final values in cartesian\n";
-        std::cout << "time = " << p.rdata(PIdx::time)  << "\n";
-        std::cout << "x    = " << p.rdata(PIdx::x)    << "\n";
-        std::cout << "y    = " << p.rdata(PIdx::y)    << "\n";
-        std::cout << "z    = " << p.rdata(PIdx::z)    << "\n";
-        std::cout << "pt   = " << p.rdata(PIdx::pupt)  << "\n";
-        std::cout << "px   = " << p.rdata(PIdx::pupx) << "\n";
-        std::cout << "py   = " << p.rdata(PIdx::pupy) << "\n";
-        std::cout << "pz   = " << p.rdata(PIdx::pupz) << "\n";
-        
+    
 
 
-        
+    //Cylindrical test
+    //******************
 
-    }
+    CylindricalMetric metric2;
+
+    p=p1;
+    
+    Euler(p, metric2, dt, steps); //Euler does the time integration
+    
+    // calculating volume. Syntax: vol(xmax,xmin,ymax,ymin,zmax,zmin). 
+    v=metric2.vol(4,2,3,-1,5,1);  
+
+    
+    tol = 1e-2; //setting tolerance for assert
+
+    //asserting the correct final result
+    assert(std::abs(p.rdata(PIdx::time)- 5.0) < tol);
+    assert(std::abs(p.rdata(PIdx::x) - 13.0 / 3.0) < tol);
+    assert(std::abs(p.rdata(PIdx::y) - 8.0 / 3.0) < tol);
+    assert(std::abs(p.rdata(PIdx::z) - 13.0 / 3.0) < tol);
+    assert(std::abs(p.rdata(PIdx::pupt) - 3.0) < tol);
+    assert(std::abs(p.rdata(PIdx::pupx) - 2.0) < tol);
+    assert(std::abs(p.rdata(PIdx::pupy) - 1.0) < tol);
+    assert(std::abs(p.rdata(PIdx::pupz) - 2.0) < tol);
+    assert(std::abs(v - 96.0) < 1e-3);
+
+    metric2.coord_conv(p); //converting the 4-position and 4-momentum into cylindrical coordinates
+
+    
+    // Printing out final state values and volume
+    std::cout << "final values in cylindrical coordinates\n";
+    std::cout << "time = " << p.rdata(PIdx::time)  << "\n";
+    std::cout << "x    = " << p.rdata(PIdx::x)    << "\n";
+    std::cout << "y    = " << p.rdata(PIdx::y)    << "\n";
+    std::cout << "z    = " << p.rdata(PIdx::z)    << "\n";
+    std::cout << "pt   = " << p.rdata(PIdx::pupt)  << "\n";
+    std::cout << "px   = " << p.rdata(PIdx::pupx) << "\n";
+    std::cout << "py   = " << p.rdata(PIdx::pupy) << "\n";
+    std::cout << "pz   = " << p.rdata(PIdx::pupz) << "\n";
+    std::cout << "v   = " << v << "\n";
+
+    metric2.coord_conv_inv(p);
+
+
+
+
+    //Spherical test
+    //******************
+
+    SphericalMetric metric3;
+
+    p=p1;
+    
+    Euler(p, metric3, dt, steps); //Euler does the time integration
+    
+    // calculating volume. Syntax: vol(xmax,xmin,ymax,ymin,zmax,zmin). 
+    v=metric3.vol(4,2,3,-1,5,1);  
+    
+
+    //asserting the correct final result
+    assert(std::abs(p.rdata(PIdx::time)- 5.0) < tol);
+    assert(std::abs(p.rdata(PIdx::x) - 13.0 / 3.0) < tol);
+    assert(std::abs(p.rdata(PIdx::y) - 8.0 / 3.0) < tol);
+    assert(std::abs(p.rdata(PIdx::z) - 13.0 / 3.0) < tol);
+    assert(std::abs(p.rdata(PIdx::pupt) - 3.0) < tol);
+    assert(std::abs(p.rdata(PIdx::pupx) - 2.0) < tol);
+    assert(std::abs(p.rdata(PIdx::pupy) - 1.0) < tol);
+    assert(std::abs(p.rdata(PIdx::pupz) - 2.0) < tol);
+    assert(std::abs(v - 114.2616) < 1e-3);
+
+    metric3.coord_conv(p); //converting the 4-position and 4-momentum into spherical coordinates
+
+
+    
+    // Printing out final state values and volume
+    std::cout << "final values in spherical coordinates\n";
+    std::cout << "time = " << p.rdata(PIdx::time)  << "\n";
+    std::cout << "x    = " << p.rdata(PIdx::x)    << "\n";
+    std::cout << "y    = " << p.rdata(PIdx::y)    << "\n";
+    std::cout << "z    = " << p.rdata(PIdx::z)    << "\n";
+    std::cout << "pt   = " << p.rdata(PIdx::pupt)  << "\n";
+    std::cout << "px   = " << p.rdata(PIdx::pupx) << "\n";
+    std::cout << "py   = " << p.rdata(PIdx::pupy) << "\n";
+    std::cout << "pz   = " << p.rdata(PIdx::pupz) << "\n";
+    std::cout << "v   = " << v << "\n";
+
+    metric3.coord_conv_inv(p);
     
 
 
