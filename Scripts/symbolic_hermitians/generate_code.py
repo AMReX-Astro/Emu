4f30dd27cf3d3b53f3622e788438531aac80d126
erickurquilla <erickurq@gmail.com>
2026-05-13 20:33:00 -0400

    to optimize memory just generate variable to set equilibrium when is indicaed in compilation via the flag SET_EQUILIBRIUM

--- Scripts/symbolic_hermitians/generate_code.py
+++ Scripts/symbolic_hermitians/generate_code.py
@@ -14,6 +14,7 @@ parser.add_argument("-eh", "--emu_home", type=str, default=".", help="Path to Em
 parser.add_argument("-c", "--clean", action="store_true", help="Clean up any previously generated files.")
 parser.add_argument("-rn", "--rhs_normalize", action="store_true", help="Normalize F when applying the RHS update F += dt * dFdt (limits to 2nd order in time).")
 parser.add_argument("-nm", "--num_moments", type=int, default=2, help="Number of moments to compute.")
+parser.add_argument("--set_equilibrium", type=int, choices=[0,1], default=0, help="If 1, set equilibrium matrix elements to initial values; if 0, do not.")
 
 args = parser.parse_args()
 
@@ -102,10 +103,11 @@ if __name__ == "__main__":
             A = HermitianMatrix(args.N, v+"{}{}_{}"+t)
             code += A.header()
 
-    for t in tails:
-        for v in vars:
-            A = HermitianMatrix(args.N, v+"{}{}_{}"+t+"_eq")
-            code += A.header()
+    if args.set_equilibrium:
+        for t in tails:
+            for v in vars:
+                A = HermitianMatrix(args.N, v+"{}{}_{}"+t+"_eq")
+                code += A.header()
 
     code += ["TrHN"]
     code += ["Vphase"]
