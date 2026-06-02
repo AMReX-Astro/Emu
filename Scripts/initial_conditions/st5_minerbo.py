ab07943e094ab371eb9eae8a8e15c8ab4523465a
erickurquilla <erickurq@gmail.com>
2026-06-02 13:21:01 -0400

    setting phase space volume in st5 initial condition script

--- Scripts/initial_conditions/st5_minerbo.py
+++ Scripts/initial_conditions/st5_minerbo.py
@@ -19,6 +19,9 @@ fnue = np.array([0.0974572, 0.04217632, -0.13433261])
 fnua = np.array([0.07237959, 0.03132354, -0.3446878])
 fnux = np.array([-0.02165833, 0.07431613, -0.53545951])
 energy_erg = 20.05473294163565 * 1e6*amrex.eV
+energies_top_erg = 20.953 * 1e6*amrex.eV
+energies_bottom_erg = 15.691 * 1e6*amrex.eV
+n_directions = 92
 
 nnu = np.zeros((2,NF))
 nnu[0,0] = nnue
@@ -34,6 +37,27 @@ particles = moment_interpolate_particles(nphi_equator, nnu, fnu, energy_erg, uni
 
 # Get variable keys
 rkey, ikey = amrex.get_particle_keys(NF, ignore_pos=True)
+
+# Setting the equilibrium number densities equal to the initial number densities
+set_equilibrium_initial_conditions = 1
+if set_equilibrium_initial_conditions == 1:
+    if "N00_Re_eq" in rkey:
+        for nu_nubar, suffix in zip(range(2), ["","bar"]):
+            for flavor in range(NF):
+                fvarname = "N"+str(flavor)+str(flavor)+"_Re"+suffix
+                particles[:, rkey[fvarname+"_eq"]] = particles[:, rkey[fvarname]]
+
+# Set initial conditions to vacuum
+set_vacuum_initial_conditions = 0
+if set_vacuum_initial_conditions == 1:
+    for nu_nubar, suffix in zip(range(2), ["","bar"]):
+        for flavor in range(NF):
+            fvarname = "N"+str(flavor)+str(flavor)+"_Re"+suffix
+            particles[:, rkey[fvarname]] = 1e10 * np.random.random(particles.shape[0])
+
+
+Vphase = ( 4.0 * np.pi / n_directions ) * ( ( energies_top_erg ** 3 - energies_bottom_erg ** 3 ) / 3.0 )
+particles[:,-1] = Vphase
 
 # Add header labels as first row
 header = np.array(list(rkey.keys()), dtype=object)
