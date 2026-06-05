37b58ef775b43c5aeacb1ec4842e7b884edc3463
erickurquilla <erickurq@gmail.com>
2026-06-05 16:24:13 -0400

    delete flag to set equilibrium

--- Scripts/symbolic_hermitians/generate_code.py
+++ Scripts/symbolic_hermitians/generate_code.py
@@ -15,7 +15,6 @@ parser.add_argument("-eh", "--emu_home", type=str, default=".", help="Path to Em
 parser.add_argument("-c", "--clean", action="store_true", help="Clean up any previously generated files.")
 parser.add_argument("-rn", "--rhs_normalize", action="store_true", help="Normalize F when applying the RHS update F += dt * dFdt (limits to 2nd order in time).")
 parser.add_argument("-nm", "--num_moments", type=int, default=2, help="Number of moments to compute.")
-parser.add_argument("--set_equilibrium", type=int, choices=[0,1], default=0, help="If 1, set equilibrium matrix elements to initial values; if 0, do not.")
 
 args = parser.parse_args()
 
