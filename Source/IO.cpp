03dda767932944e5779745ea6a889092238c47ed
Sherwood Richers <richers@utk.edu>
2026-08-31 11:59:17 -0400

    remove features that limit scaling efficiency. The calculate_dt and outputting the grid take a huge amount of time on more than a few hundred GPUs on Frontier.

--- Source/IO.cpp
+++ Source/IO.cpp
@@ -38,20 +38,14 @@ void WritePlotFile(const amrex::MultiFab& state,
                                     time, step);
 #endif
 
-    FlavoredNeutrinoContainer neutrinos_for_IO(geom, state.DistributionMap(),
-                                               state.boxArray());
-    neutrinos_for_IO.copyParticles(neutrinos, true);
-
     if (write_plot_particles == 1) {
         auto neutrino_varnames = neutrinos.get_attribute_names();
 #ifdef AMREX_USE_HDF5
         neutrinos.CheckpointHDF5(plotfilename, "neutrinos", true,
                                  neutrino_varnames);
-        // neutrinos_for_IO.WritePlotFile(plotfilename, "neutrinos", neutrino_varnames); // This is the way to write the particles to the plt file that work for parawview visualization
 #else
         neutrinos.Checkpoint(plotfilename, "neutrinos", true,
                              neutrino_varnames);
-        // neutrinos_for_IO.WritePlotFile(plotfilename, "neutrinos", neutrino_varnames); // This is the way to write the particles to the plt file that work for parawview visualization
 #endif
     }
 
